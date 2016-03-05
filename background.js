fa4b51aa899ce91704cadc169b3f388e6967b2d3
Jameson Weng <weng.jameson126@gmail.com>
2016-03-05 07:46:58 -0500

    Accessing reminder list

--- background.js
+++ background.js
@@ -1,11 +1,13 @@
-chrome.alarms.onAlarm.addListener(function(alarm) {
-	var reminders = getReminders();
-	var currDate = new Date();
-	counter = 0;
+var reminders = [{time: new Date(), message: "Kevin is beside me"}];
 
-	while (counter < reminders.length() && currTime <= reminder[counter].time) {
-		alert(reminder[counter].message);
-		deleteReminder(counter);
+chrome.alarms.onAlarm.addListener(function(alarm) {
+	alert("hi");
+
+	var currDate = new Date();
+	var counter = 0;
+	
+	while (counter < reminders.length && currDate >= reminders[counter].time) {
+		alert(reminders[counter].message);
 		counter += 1;
 	}
 });
@@ -13,3 +15,4 @@ chrome.alarms.onAlarm.addListener(function(alarm) {
 chrome.windows.onRemoved.addListener(function(windowId) {
 	save();
 })
+	
